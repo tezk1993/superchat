168af97d69e91d2c118ba7e4e72bd455b4f71ad4
busser1993 <sickogagi@msn.com>
2024-12-02 02:38:18 +0100

    Remove photo if there is no url, reverse order of messages

--- src/Components/ChatMessage/ChatMessage.jsx
+++ src/Components/ChatMessage/ChatMessage.jsx
@@ -1,15 +1,17 @@
-import React from 'react'
-import {app,auth,firestore} from '../../firebase';
+import React from "react";
+import { app, auth, firestore } from "../../firebase";
 import styles from "./ChatMessage.module.css";
 
 export const ChatMessage = (props) => {
-  const {text, uid,photoURL} = props.message;
-
+  const { text, uid, photoURL } = props.message;
 
   return (
-    <div className={uid === auth.currentUser.uid ? styles.sent : styles.received} id={styles.message}>
-      <img src={photoURL} alt='user image'/>
+    <div
+      className={uid === auth.currentUser.uid ? styles.sent : styles.received}
+      id={styles.message}
+    >
+      {photoURL ? <img src={photoURL} alt="user image" /> : <></>}
       <p>{text}</p>
     </div>
-  )
-}
+  );
+};
--- src/Components/ChatRoom/ChatRoom.jsx
+++ src/Components/ChatRoom/ChatRoom.jsx
@@ -1,49 +1,61 @@
-import React from 'react'
+import React from "react";
 import styles from "./ChatRoom.module.css";
-import { ChatMessage } from '../ChatMessage/ChatMessage';
-import {app,auth,firestore} from '../../firebase';
-import {useCollectionData} from 'react-firebase-hooks/firestore';
-import { useState } from 'react';
-import firebase from 'firebase/compat/app'; 
-import { useRef } from 'react';
+import { ChatMessage } from "../ChatMessage/ChatMessage";
+import { app, auth, firestore } from "../../firebase";
+import { useCollectionData } from "react-firebase-hooks/firestore";
+import { useState } from "react";
+import firebase from "firebase/compat/app";
+import { useRef } from "react";
 
 export const ChatRoom = () => {
-    const messagesRef = firestore.collection('messages');
-    const query = messagesRef.orderBy('createdAt').limit(25);
-  
-    const [messages] = useCollectionData(query, {idField: 'id'});
-  
-    const [formValue, setFormValue] = useState('');
-    const dummy = useRef();
-    const sendMessage = async(e) =>{
-      e.preventDefault();
-      if(formValue === "") return;
-      const {uid,photoURL} = auth.currentUser;
-  
-      await messagesRef.add({
-        text:formValue,
-        createdAt: firebase.firestore.FieldValue.serverTimestamp(),
-        uid,
-        photoURL
-      });
-  
-      setFormValue('');
-  
-      dummy.current.scrollIntoView({behavior: 'smooth'});
-    }
-    
-    return (
-      <div className={styles.container}>
-        <main className={styles.messages}>
-            {messages && messages.map(msg => <ChatMessage key={msg.id} message={msg}/>)}
-        </main>
-        <div>
-          <form  onSubmit={sendMessage} ref={dummy}>
-            <input className={styles.textinput} value={formValue} onChange={(e) => setFormValue(e.target.value)}/>
-            <button disabled={formValue === "" ? true : false} id={styles.submitbutton}type='submit'>Send <i class="fa fa-arrow-right" aria-hidden="true"></i></button>
-          </form>
-        </div>
+  const messagesRef = firestore.collection("messages");
+  const query = messagesRef.orderBy("createdAt").limit(25);
+
+  const [messages] = useCollectionData(query, { idField: "id" });
+
+  const [formValue, setFormValue] = useState("");
+  const dummy = useRef();
+  const sendMessage = async (e) => {
+    e.preventDefault();
+    if (formValue === "") return;
+    const { uid, photoURL } = auth.currentUser;
+
+    await messagesRef.add({
+      text: formValue,
+      createdAt: firebase.firestore.FieldValue.serverTimestamp(),
+      uid,
+      photoURL,
+    });
+
+    setFormValue("");
+
+    dummy.current.scrollIntoView({ behavior: "smooth" });
+  };
+
+  return (
+    <div className={styles.container}>
+      <main className={styles.messages}>
+        {messages &&
+          messages
+            .reverse()
+            .map((msg) => <ChatMessage key={msg.id} message={msg} />)}
+      </main>
+      <div>
+        <form onSubmit={sendMessage} ref={dummy}>
+          <input
+            className={styles.textinput}
+            value={formValue}
+            onChange={(e) => setFormValue(e.target.value)}
+          />
+          <button
+            disabled={formValue === "" ? true : false}
+            id={styles.submitbutton}
+            type="submit"
+          >
+            Send <i class="fa fa-arrow-right" aria-hidden="true"></i>
+          </button>
+        </form>
       </div>
-  )
-  
-}
+    </div>
+  );
+};
